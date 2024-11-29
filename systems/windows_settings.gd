40bdb1e49c673c0450383143f83ae8b1d09f4fe9
Alexander-Jordan <curtjordan93@gmail.com>
2024-11-29 13:01:11 +0100

    Adjust window_settings, move right wall

--- systems/windows_settings.gd
+++ systems/windows_settings.gd
@@ -1,21 +1,20 @@
 extends Control
 
-# Set and Enforce a Minimum Wdith & Height by adjusting these two values 720 x 1280
-# This will not change your overall project settings merely enforce a project settings minimum size 
+# set and enforce a minimum width & height by adjusting these two values
+# this will not change your overall project settings
+# merely enforce a project settings minimum size 
 var min_size = Vector2i(720, 1280)
+@onready var window:Window = get_viewport().get_window()
 
 func _ready():
-	# Set the minimum size in project settings
+	# set the minimum size in project settings
 	ProjectSettings.set_setting("display/window/size/min_width", min_size.x)
 	ProjectSettings.set_setting("display/window/size/min_height", min_size.y)
 	
-	# Enforce the minimum window size using DisplayServer
-	DisplayServer.window_set_min_size(min_size)
+	# enforce the minimum window size using DisplayServer
+	window.min_size = min_size
 
 func _process(_delta):
-	# Get the current window size
-	var current_size = DisplayServer.window_get_size()
-
-	# Only enforce minimum size without adjusting the window size if it's above the min size
-	if current_size.x < min_size.x or current_size.y < min_size.y:
-		DisplayServer.window_set_size(Vector2i(max(current_size.x, min_size.x), max(current_size.y, min_size.y)))
+	# only enforce minimum size without adjusting the window size if it's above the min size
+	if window.size.x < min_size.x or window.size.y < min_size.y:
+		window.size = Vector2i(maxi(window.size.x, min_size.x), maxi(window.size.y, min_size.y))
