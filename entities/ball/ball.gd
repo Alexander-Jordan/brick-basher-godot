1e47f71ae080585475358ab84c9f8d43041163eb
Alexander-Jordan <curtjordan93@gmail.com>
2024-12-04 08:23:44 +0100

    Add ball reset/serve functionality, make the ball follow the paddle before serving, and add custom bounce function to the paddle

---
+++ entities/ball/ball.gd
@@ -0,0 +1,38 @@
+class_name Ball
+extends CharacterBody2D
+
+@export var speed:int = 200
+var rng = RandomNumberGenerator.new()
+var direction:Vector2 = Vector2.ZERO
+
+signal reset_ball
+
+func reset() -> void:
+	velocity = Vector2.ZERO
+	reset_ball.emit()
+
+func serve():
+	direction = Vector2(rng.randf_range(-1, 1), -1)
+	velocity = direction.normalized()
+ 
+func _physics_process(delta: float) -> void:
+	var collision = move_and_collide(velocity * speed * delta)
+	if collision:
+		var collider:Object = collision.get_collider()
+		if !handle_velocity_after_collision(collider):
+			# if the velocity was not handled after collision, just bounce
+			velocity = velocity.bounce(collision.get_normal())
+
+func handle_velocity_after_collision(object:Object) -> bool:	
+	# then check if the velocity should be handled
+	if object.has_method("ball_velocity_after_bounce"):
+		velocity = object.ball_velocity_after_bounce(velocity, position)
+		return true
+	
+	# returning false means that the velocity was not handled
+	return false
+
+func _on_screen_exited() -> void:
+	# wait for some time before doing anything else
+	await get_tree().create_timer(1.0).timeout
+	reset()
--- entities/paddle/paddle.gd
+++ entities/paddle/paddle.gd
@@ -1,8 +1,46 @@
 extends CharacterBody2D
 
-var speed:int = 2000
+@export var ball:Ball
 
-func _process(delta: float) -> void:
-	var input_pos:Vector2 = get_viewport().get_mouse_position()
+var input_pos:Vector2 = self.position
+var speed:int = 2000
+var lock_ball:bool = true
+
+signal spawn_ball()
+
+func _ready() -> void:
+	if ball:
+		ball.reset_ball.connect(func(): lock_ball = true)
+
+func _physics_process(delta: float) -> void:
+	input_pos = get_viewport().get_mouse_position()
 	velocity = Vector2(input_pos.x - position.x, 0) * speed * delta
 	move_and_slide()
+	
+	if lock_ball:
+		lock_ball_to_paddle()
+		if Input.is_action_just_pressed('serve'):
+			serve_ball()
+
+func lock_ball_to_paddle() -> void:
+	if ball:
+		ball.position = Vector2(position.x, position.y - 20)
+
+func serve_ball() -> void:
+	if ball and lock_ball:
+		lock_ball = false
+		ball.serve()
+
+func ball_velocity_after_bounce(ball_velocity:Vector2, ball_position:Vector2) -> Vector2:
+	# new y should be:
+	# - negative if the ball on impact is from the top of the paddle
+	# - positive if the ball on impact is from the bottom of the paddle
+	var new_y:float = -abs(ball_velocity.y) if ball_position.y <= position.y else abs(ball_velocity.y)
+	# how far from the center in x-axis was the hit?
+	var from_center:float = ball_position.x - position.x
+	# new x is calculated by normalizing from_center (ex. 20 to -20 range -> 1 to -1 range)
+	# that's done by dividing from_center with max value (in this case: width / 2)
+	var new_x:float = from_center / ((6 * scale.x) / 2)
+	# clamp new_x so it doesn't shoot straight left or right
+	new_x = clampf(new_x, -0.9, 0.9)
+	return Vector2(new_x, new_y).normalized()
