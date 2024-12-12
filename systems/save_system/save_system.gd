2067e1b4ff3807fa41705fccfa76412656ebcfc6
Alexander-Jordan <curtjordan93@gmail.com>
2024-12-12 15:53:48 +0100

    Add variables to save_system

--- systems/save_system/save_system.gd
+++ systems/save_system/save_system.gd
@@ -11,6 +11,9 @@ const SETTINGS_DEFAULT:Dictionary = {
   }
 }
 
+var settings_data:Dictionary = SETTINGS_DEFAULT
+var stats_data:StatsData = StatsData.new()
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
