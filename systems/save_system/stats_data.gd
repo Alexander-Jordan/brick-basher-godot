d0409dd437a9e04b4eb5eb157524169926255f88
Alexander-Jordan <curtjordan93@gmail.com>
2024-12-12 15:46:44 +0100

    Add exported variables in stats_data

--- systems/save_system/stats_data.gd
+++ systems/save_system/stats_data.gd
@@ -1,2 +1,11 @@
 class_name StatsData
 extends Resource
+
+@export var game_booted_count:int = 0
+@export var games_count:int = 0
+@export var last_score:int = 0
+@export var best_score:int = 0
+@export var balls_served_count:int = 0
+@export var bricks_destroyed_count:int = 0
+@export var screens_cleared_count:int = 0
+@export var entered_hard_mode_count:int = 0
